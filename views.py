c99a157b4c2347a8e55a3c3ed712b6570269cf55
Haicheng Charles Zhao <czhao39@gmail.com>
2016-07-10 20:22:35 -0400

    define IndexView

--- views.py
+++ views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Project, Skill
+
+
+class IndexView(generic.ListView):
+    template_name = "portfolio/index.html"
+
+    def get_context_data(self):
+        context = super(IndexView, self).get_context_data()
+        context["project_list"] = Project.objects.order_by("date")
+        context["skill_list"] = Skill.objects.order_by("proficiency")
+        return context
