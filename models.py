4cb160cec55b02534a527f3e82b84d5a451f1b03
Haicheng Charles Zhao <czhao39@gmail.com>
2016-07-10 21:17:24 -0400

    created Project and Skill models

--- models.py
+++ models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=20)
+    year = models.PositiveSmallIntegerField()
+    picture = models.ImageField(upload_to="img/")
+    details = models.TextField()
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        ordering = ["-year", "name"]
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=20)
+    proficiency = models.PositiveSmallIntegerField(choices=((1, 1), (2, 2), (3, 3), (4, 4), (5, 5)))
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        ordering = ["-proficiency", "name"]
