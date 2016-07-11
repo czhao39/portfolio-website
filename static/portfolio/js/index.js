be0fd532cca39e18bdd9ed021151c7b2ca09f5b1
Haicheng Charles Zhao <czhao39@gmail.com>
2016-07-11 18:20:46 -0400

    summary section with typed and particles

--- static/portfolio/js/index.js
+++ static/portfolio/js/index.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+    $(".typed").typed({
+        strings: ["first", "second"],
+        typeSpeed: 40,
+        backSpeed: 20,
+        loop: true,
+    });
+    particlesJS.load("particles-js", "/static/portfolio/particlesjs-config.json", function() {
+        console.log("particles loaded");
+    });
     $(".button-collapse").sideNav();
-})
+});
