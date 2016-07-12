3575648d896e1ff6c2e893fa68a561cb1faf7913
Haicheng Charles Zhao <czhao39@gmail.com>
2016-07-12 09:40:11 -0400

    intro section fade in

--- static/portfolio/js/index.js
+++ static/portfolio/js/index.js
@@ -8,11 +8,18 @@ $(document).ready(function() {
         backDelay: 1000,
         loop: true,
     });
+    
     particlesJS.load("particles-js", "/static/portfolio/particlesjs-config.json");
+    
+    $(".intro-content").fadeIn(1500).animate({top: 90}, {queue: false, duration: 1000});
+
     $(".button-collapse").sideNav({closeOnClick: true});
+    
     $(".scrollspy").scrollSpy();
     var sr = ScrollReveal();
+    sr.reveal(".section");
     sr.reveal("#skills tr");
     sr.reveal(".card");
+    
     $('.modal-trigger').leanModal();
 });
