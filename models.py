6a8d5b9c06687fafbeebabee1a898d64622dca82
Haicheng Charles Zhao <czhao39@gmail.com>
2017-04-30 11:05:45 -0400

    make skill category rank editable

--- models.py
+++ models.py
@@ -22,7 +22,7 @@ class Project(models.Model):
 
 class SkillCategory(models.Model):
     name = models.CharField(max_length=20)
-    rank = models.AutoField(primary_key=True)
+    rank = models.PositiveSmallIntegerField(primary_key=True)
 
     def __str__(self):
         return self.name
