0e12ec397eaeffc63dab8256586249a2c778eda6
Haicheng Charles Zhao <czhao39@gmail.com>
2017-05-07 22:26:51 -0400

    add fancy pendulum animation at bottom

--- static/portfolio/js/index.js
+++ static/portfolio/js/index.js
@@ -1,9 +1,17 @@
+const FANCY_LEN = 10000;
+const MIN_OSC = 1;
+const NUM_CIRCS = 12;
+
+var count;
+var $circles;
+
+
 $(document).ready(function() {
     $window = $(window);
     $navbar = $(".navbar");
-    
+
     var height = Math.max($window.height(), $(".intro-content").height()+330);
-    
+
     $(".page-section").height(height);
     $("#skills").css("margin-top", height+10);
 
@@ -14,12 +22,12 @@ $(document).ready(function() {
         backDelay: 1000,
         loop: true,
     });
-    
+
     if ($window.width() <= 640)
         particlesJS.load("particles-js", "/static/portfolio/js/particlesjs-config-mobile.json");
     else
         particlesJS.load("particles-js", "/static/portfolio/js/particlesjs-config.json");
-    
+
     $(".intro-content").velocity({top: "25%", opacity: 1}, {duration: 900, complete: function() {
         $navbar.css("top", "-20px");
         $navbar.velocity({top: 0, opacity: 1}, {duration: 800});
@@ -45,6 +53,8 @@ $(document).ready(function() {
 
     var sr = ScrollReveal();
     sr.reveal(".card", {viewFactor: 0.1, duration: 1000});
+
+    initialize_fancy();
 });
 
 function underline(el) {
@@ -59,4 +69,37 @@ function loadProgress() {
         $el.css("visibility", "visible");
         $el.velocity({width: the_width}, {duration: 1000, easing: "easeOutCubic"});
     });
+}
+
+function initialize_fancy() {
+    var fancy_wrapper = document.getElementById("fancy_wrapper");
+    const radius = 90 / 3 / NUM_CIRCS;
+    for (let i=0; i<NUM_CIRCS; i++) {
+        let circ = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        circ.setAttribute("cx", radius*3*(i+1));
+        circ.setAttribute("cy", 10);
+        circ.setAttribute("r", radius);
+        fancy_wrapper.appendChild(circ);
+    }
+
+    $circles = $("#fancy_wrapper circle");
+    count = NUM_CIRCS;
+    cycle();
+};
+
+function cycle() {
+    count++;
+    if (count >= NUM_CIRCS) {
+        count = 0;
+        let r = (155 + Math.round(Math.random()*100)).toString(16);
+        if (r.length === 1) r = "0" + r;
+        let g = (Math.round(Math.random()*200)).toString(16);
+        if (g.length === 1) g = "0" + g;
+        let b = (Math.round(Math.random()*200)).toString(16);
+        if (b.length === 1) b = "0" + b;
+        let color = "#" + r + g + b;
+        $circles.each(function(ind) {
+            $(this).velocity({cy: "20", fill: color}, {duration: FANCY_LEN/(MIN_OSC+ind)/2.0, loop: MIN_OSC+ind, complete: cycle});
+        });
+    }
 }
